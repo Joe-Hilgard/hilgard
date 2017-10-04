06fa743a6ca829f6f0727443b5844757e817d9c3
Joe-Hilgard <jhilgard@gmail.com>
2017-10-04 17:38:40 -0500

    Tentative new function report_LRchisq

---
+++ R/report_LRchisq.R
@@ -0,0 +1,23 @@
+#' Report a likelihood-ratio chisq-test
+#'
+#' Report an LR chisq-test as chisq(df) = chisq, p = .###.
+#' @param model Model object from which to extract chisq-value.
+#' @param effect Parameter with chisq-value of interest
+#' @template imports
+#'
+#' @export
+
+report_LRchisq <- function(model, effect) {
+
+  frame <- tidy(car::Anova(model, type = 3))
+  df <- with(frame, df[term == effect])
+
+  chisq <- with(frame, LR.Chisq[term == effect])
+  chisq <- round(chisq, 2)
+
+  p <- with(frame, p.value[term == effect])
+  p <- fix_p(p)
+
+  output <- paste0("*chisq*(", df, ") = ", chisq, ", ", p)
+  return(output)
+}
