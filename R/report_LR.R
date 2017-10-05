c588177feeea235751cecd4096c426e11a05174b
Joe Hilgard <jhilgard@gmail.com>
2017-10-04 22:21:30 -0500

    Made super crappy chisq reporter function

---
+++ R/report_LR.R
@@ -0,0 +1,22 @@
+#' Report manually-made likelihood-ratio chisq-test
+#'
+#' Report an LR chisq-test as chisq(df) = chisq, p = .###.
+#' @param model Model object from which to extract chisq-value.
+#' @param effect Parameter with chisq-value of interest
+#' @template imports
+#'
+#' @export
+
+report_LR <- function(row) {
+
+  df <- row$df
+
+  chisq <- row$LR.chisq
+  chisq <- round(chisq, 2)
+
+  p <- row$p.value
+  p <- fix_p(p)
+
+  output <- paste0("*chisq*(", df, ") = ", chisq, ", ", p)
+  return(output)
+}
